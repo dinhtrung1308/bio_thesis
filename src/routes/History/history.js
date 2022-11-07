aa0c1771680cea72c229af1aac9fa0f48bfc8e5e
dinhtrung1308 <66743192+dinhtrung1308@users.noreply.github.com>
2022-11-08 00:43:24 +0700

    code 8/11

--- src/routes/History/history.js
+++ src/routes/History/history.js
@@ -10,9 +10,18 @@ import { Button, Menu } from "antd";
 import moment from "moment";
 import React, { useState, useEffect } from "react";
 import "./history.css";
-import { Space, Typography, DatePicker, List } from "antd";
+import { Space, Typography, DatePicker, List, Modal } from "antd";
 import { Chart, registerables } from "chart.js";
 import { Line } from "react-chartjs-2";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
 
 import { Col, Row } from "antd";
 import { Card } from "antd";
@@ -26,6 +35,7 @@ import {
   updateDoc,
   deleteDoc,
   doc,
+  where,
 } from "firebase/firestore";
 import { db, db2 } from "../../firebase-config.js";
 
@@ -35,6 +45,11 @@ Chart.register(...registerables);
 const { Title } = Typography;
 const { RangePicker } = DatePicker;
 const { Meta } = Card;
+
+const currentDate = moment().format("DD-MM-YYYY").split("-");
+const currentDay = currentDate[0];
+const currentMonth = currentDate[1];
+const currentYear = currentDate[2];
 
 function getItem(label, key, icon, children, type) {
   return {
@@ -49,6 +64,65 @@ function checkCondition(sys, dia) {
   if (sys <= 90 || dia <= 60) return "Huyết Áp Thấp";
   else if (sys <= 120 || dia <= 80) return "Huyết Áp Bình Thường";
   else if (sys > 120 || dia > 80) return " Huyết Áp Cao";
+}
+function convertArrayToDays(data) {
+  let labels = [];
+
+  for (let i = 0; i < data.length; i++) {
+    labels.push(moment.unix(data[i]?.insert_at.seconds).format("HH:mm:ss"));
+  }
+  return labels;
+}
+function convertArrayToSYS(data) {
+  let sys = [];
+
+  for (let i = 0; i < data.length; i++) {
+    sys.push(data[i].sys);
+  }
+  return sys;
+}
+function convertArrayToDIA(data) {
+  let dia = [];
+
+  for (let i = 0; i < data.length; i++) {
+    dia.push(data[i].dia);
+  }
+  return dia;
+}
+function convertArrayToHeartRate(data) {
+  let heart_rate = [];
+
+  for (let i = 0; i < data.length; i++) {
+    heart_rate.push(data[i].heart_rate);
+  }
+  return heart_rate;
+}
+function calculateAverageSYS(data) {
+  let sum = 0;
+  let avg = 0;
+  for (let i = 0; i < data.length; i++) {
+    sum += Number(data[i].sys);
+  }
+  avg = sum / data.length;
+  return avg;
+}
+function calculateAverageDIA(data) {
+  let sum = 0;
+  let avg = 0;
+  for (let i = 0; i < data.length; i++) {
+    sum += Number(data[i].dia);
+  }
+  avg = sum / data.length;
+  return avg;
+}
+function calculateAverageHeartRate(data) {
+  let sum = 0;
+  let avg = 0;
+  for (let i = 0; i < data.length; i++) {
+    sum += Number(data[i].heart_rate);
+  }
+  avg = sum / data.length;
+  return avg;
 }
 
 const items = [
@@ -68,205 +142,32 @@ const options = {
     },
   },
 };
-const data = [
-  {
-    title: "Title 1",
-  },
-  {
-    title: "Title 2",
-  },
-  {
-    title: "Title 3",
-  },
-  {
-    title: "Title 4",
-  },
-  {
-    title: "Title 4",
-  },
-  {
-    title: "Title 4",
-  },
-  {
-    title: "Title 4",
-  },
-];
-const labels = [
-  "23:42:48",
-  "23:46:16",
-  "00:13:23",
-  "20:03:41",
-  "20:04:45",
-  "22:22:55",
-];
 
-const data2 = {
-  labels,
-  datasets: [
-    {
-      label: "SYS",
-      data: [123, 123, 123, 123, 60, 60],
-      borderColor: "rgb(126, 229, 0)",
-      backgroundColor: "rgba(126, 229, 0, 0.5)",
-    },
-    {
-      label: "DIA",
-      data: [91, 91, 12, 75, 85, 85],
-      borderColor: "rgb(53, 162, 235)",
-      backgroundColor: "rgba(53, 162, 235, 0.5)",
-    },
-    {
-      label: "Heart rate",
-      data: [43, 43, 43, 43, 43, 43],
-      borderColor: "rgb(255, 99, 132)",
-      backgroundColor: "rgba(255, 99, 132, 0.5)",
-    },
-  ],
-};
-const records = [
-  {
-    id: "1",
-    date: "08/04/2022",
-    hour: "10:19",
-    status: "Huyết Áp Bình Thường",
-    sys: 136,
-    dia: 78,
-    heartrate: 83,
-  },
-  {
-    id: "2",
-    date: "09/04/2022",
-    hour: "10:31",
-    status: "Huyết Áp Bình Thường",
-    sys: 113,
-    dia: 60,
-    heartrate: 83,
-  },
-  {
-    id: "3",
-    date: "09/04/2022",
-    hour: "15:20",
-    status: "Huyết Áp Bình Thường",
-    sys: 119,
-    dia: 68,
-    heartrate: 83,
-  },
-  {
-    id: "4",
-    date: "09/04/2022",
-    hour: "16:42",
-    status: "Huyết Áp Bình Thường",
-    sys: 125,
-    dia: 71,
-  },
-  {
-    id: "5",
-    date: "10/04/2022",
-    hour: "09:30",
-    status: "Huyết Áp Bình Thường",
-    sys: 116,
-    dia: 70,
-    heartrate: 83,
-  },
-  {
-    id: "6",
-    date: "10/04/2022",
-    hour: "10:17",
-    status: "Huyết Áp Bình Thường",
-    sys: 111,
-    dia: 65,
-  },
-  {
-    id: "7",
-    date: "10/04/2022",
-    hour: "14:01",
-    status: "Huyết Áp Bình Thường",
-    sys: 112,
-    dia: 63,
-    heartrate: 83,
-  },
-  {
-    id: "8",
-    date: "10/04/2022",
-    hour: "14:59",
-    status: "Huyết Áp Bình Thường",
-    sys: 142,
-    dia: 72,
-  },
-  {
-    id: "9",
-    date: "11/04/2022",
-    hour: "09:42",
-    status: "Huyết Áp Bình Thường",
-    sys: 112,
-    dia: 62,
-    heartrate: 83,
-  },
-  {
-    id: "10",
-    date: "11/04/2022",
-    hour: "10:16",
-    status: "Huyết Áp Bình Thường",
-    sys: 106,
-    dia: 59,
-    heartrate: 83,
-  },
-  {
-    id: "11",
-    date: "11/04/2022",
-    hour: "10:40",
-    status: "Huyết Áp Bình Thường",
-    sys: 105,
-    dia: 58,
-    heartrate: 83,
-  },
-  {
-    id: "12",
-    date: "11/04/2022",
-    hour: "14:44",
-    status: "Huyết Áp Bình Thường",
-    sys: 123,
-    dia: 61,
-  },
-  {
-    id: "13",
-    date: "11/04/2022",
-    hour: "16:18",
-    status: "Huyết Áp Bình Thường",
-    sys: 111,
-    dia: 68,
-    heartrate: 83,
-  },
-  {
-    id: "14",
-    date: "12/04/2022",
-    hour: "01:22",
-    status: "Huyết Áp Bình Thường",
-    sys: 113,
-    dia: 64,
-    heartrate: 83,
-  },
-  {
-    id: "15",
-    date: "11/04/2022",
-    hour: "15:58",
-    status: "Huyết Áp Bình Thường",
-    sys: 125,
-    dia: 72,
-    heartrate: 83,
-  },
-];
 const History = () => {
   const [collapsed, setCollapsed] = useState(true);
   const [record, setRecord] = useState([]);
-
+  const [open, setOpen] = useState(false);
   let navigate = useNavigate();
   const historyCollectionRef = collection(db2, "history");
-  const q = query(historyCollectionRef, orderBy("insert_at", "desc"));
+  const q = query(
+    historyCollectionRef,
+    where(
+      "insert_at",
+      ">=",
+      new Date(`${currentYear}-${currentMonth}-${currentDay}`)
+    ),
+    where(
+      "insert_at",
+      "<=",
+      new Date(`${currentYear}-${currentMonth}-${parseInt(currentDay) + 1}`)
+    ),
+    orderBy("insert_at", "desc")
+  );
 
   useEffect(() => {
     const getUsers = async () => {
       const data = await getDocs(q);
+      console.log(data);
       setRecord(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     };
 
@@ -274,6 +175,12 @@ const History = () => {
   }, []);
   const toggleCollapsed = () => {
     setCollapsed(!collapsed);
+  };
+  const showDetail = () => {
+    setOpen(!open);
+  };
+  const handleCancel = () => {
+    setOpen(false);
   };
   function navigateToTab2(e) {
     if (e.key === "1") {
@@ -319,6 +226,7 @@ const History = () => {
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
+          paddingBottom: "20px",
         }}
       >
         <div style={{ marginTop: "10px", marginLeft: "10px" }}>
@@ -347,7 +255,32 @@ const History = () => {
               borderRadius: "40px",
             }}
           >
-            <Line options={options} data={data2} />
+            <Line
+              options={options}
+              data={{
+                labels: convertArrayToDays(record),
+                datasets: [
+                  {
+                    label: "SYS",
+                    data: convertArrayToSYS(record),
+                    borderColor: "rgb(126, 229, 0)",
+                    backgroundColor: "rgba(126, 229, 0, 0.5)",
+                  },
+                  {
+                    label: "DIA",
+                    data: convertArrayToDIA(record),
+                    borderColor: "rgb(53, 162, 235)",
+                    backgroundColor: "rgba(53, 162, 235, 0.5)",
+                  },
+                  {
+                    label: "Heart rate",
+                    data: convertArrayToHeartRate(record),
+                    borderColor: "rgb(255, 99, 132)",
+                    backgroundColor: "rgba(255, 99, 132, 0.5)",
+                  },
+                ],
+              }}
+            />
           </div>
           <Typography.Title
             level={2}
@@ -359,93 +292,142 @@ const History = () => {
             }}
           >
             {" "}
-            Bản ghi
-            <RangePicker
+            Số liệu trung bình
+            {/* <RangePicker
               onFocus={(e) => {
                 console.log(e);
               }}
-            />
+            /> */}
           </Typography.Title>
           <div style={{ marginTop: "30px", marginLeft: "10px", width: "85%" }}>
-            <List
-              grid={{
-                gutter: 16,
-                column: 1,
+            <Card
+              style={{
+                boxShadow: "rgb(0 0 0 / 35%) 0px 5px 15px",
+                borderRadius: "40px",
               }}
-              dataSource={record}
-              renderItem={(item) => (
-                <List.Item>
-                  <Card
-                    style={{
-                      boxShadow: "rgb(0 0 0 / 35%) 0px 5px 15px",
-                      borderRadius: "40px",
-                    }}
-                  >
-                    <Meta
-                      title={moment
-                        .unix(item.insert_at.seconds)
-                        .format("DD/MM/YYYY")}
-                      description={moment
-                        .unix(item.insert_at.seconds)
-                        .format("HH:mm:ss")}
-                    />
-                    <Divider></Divider>
-                    <div
+              onClick={showDetail}
+            >
+              <Meta
+                title={moment
+                  .unix(record[0]?.insert_at.seconds)
+                  .format("DD/MM/YYYY")}
+              />
+              <Divider></Divider>
+              <div
+                style={{
+                  display: "flex",
+                  flexWrap: "wrap",
+                  gap: "2rem",
+                }}
+              >
+                <div style={{ display: "flex", gap: " 50px" }}>
+                  <p style={{ fontSize: "18px" }}>
+                    Nhịp tim:{" "}
+                    <span style={{ fontWeight: 900 }}>
+                      {" "}
+                      {calculateAverageHeartRate(record)}{" "}
+                    </span>
+                    {"bpm"}
+                  </p>
+
+                  <p style={{ fontSize: "18px" }}>
+                    SYS:{" "}
+                    <span style={{ fontWeight: 900 }}>
+                      {calculateAverageSYS(record)}
+                    </span>{" "}
+                    {"mmHg"}
+                  </p>
+                  <p style={{ fontSize: "18px" }}>
+                    DIA:{" "}
+                    <span style={{ fontWeight: 900 }}>
+                      {calculateAverageDIA(record)}
+                    </span>{" "}
+                    {"mmHg"}
+                  </p>
+                </div>
+                <div>
+                  <p style={{ fontSize: "18px" }}>
+                    Trạng thái:{" "}
+                    <span
                       style={{
-                        display: "flex",
-                        gap: "10rem",
+                        color:
+                          checkCondition(
+                            calculateAverageSYS(record),
+                            calculateAverageDIA(record)
+                          ) === "Huyết Áp Thấp"
+                            ? "#7e22ff"
+                            : checkCondition(
+                                calculateAverageSYS(record),
+                                calculateAverageDIA(record)
+                              ) === "Huyết Áp Bình Thường"
+                            ? "#a7e519"
+                            : "#EE2727",
+                        fontWeight: "bold",
                       }}
                     >
-                      <div style={{ display: "flex", gap: " 50px" }}>
-                        <p style={{ fontSize: "18px" }}>
-                          Nhịp tim:{" "}
-                          <span style={{ fontWeight: 900 }}>
-                            {" "}
-                            {item.heart_rate}{" "}
-                          </span>
-                          {"bpm"}
-                        </p>
-
-                        <p style={{ fontSize: "18px" }}>
-                          SYS:{" "}
-                          <span style={{ fontWeight: 900 }}>{item.sys}</span>{" "}
-                          {"mmHg"}
-                        </p>
-                        <p style={{ fontSize: "18px" }}>
-                          DIA:{" "}
-                          <span style={{ fontWeight: 900 }}>{item.dia}</span>{" "}
-                          {"mmHg"}
-                        </p>
-                      </div>
-                      <div>
-                        <p style={{ fontSize: "18px" }}>
-                          Trạng thái:{" "}
-                          <span
-                            style={{
-                              color:
-                                checkCondition(item.sys, item.dia) ===
-                                "Huyết Áp Thấp"
-                                  ? "#7e22ff"
-                                  : checkCondition(item.sys, item.dia) ===
-                                    "Huyết Áp Bình Thường"
-                                  ? "#a7e519"
-                                  : "#EE2727",
-                              fontWeight: "bold",
-                            }}
-                          >
-                            {console.log(checkCondition(item.sys, item.dia))}
-                            {checkCondition(item.sys, item.dia)}
-                          </span>
-                        </p>
-                      </div>
-                    </div>
-                  </Card>
-                </List.Item>
-              )}
-            />
+                      {checkCondition(
+                        calculateAverageSYS(record),
+                        calculateAverageDIA(record)
+                      )}
+                    </span>
+                  </p>
+                </div>
+                <div>
+                  <p style={{ fontSize: "18px" }}>
+                    Số lần đo:{" "}
+                    <span
+                      style={{
+                        fontWeight: "bold",
+                      }}
+                    >
+                      {record.length}
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </Card>
           </div>
         </div>
       </div>
+      <Modal
+        title="Chi tiết"
+        open={open}
+        footer={null}
+        width={900}
+        onCancel={handleCancel}
+      >
+        <TableContainer component={Paper} style={{ height: 500 }}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Thời gian</TableCell>
+                <TableCell align="right">Nhịp tim</TableCell>
+                <TableCell align="right">SYS&nbsp;(g)</TableCell>
+                <TableCell align="right">DIA&nbsp;(g)</TableCell>
+                <TableCell align="right">Trạng thái&nbsp;(g)</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {record.map((item, index) => (
+                <TableRow
+                  key={index}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {moment.unix(item.insert_at.seconds).format("HH:mm:ss")}
+                  </TableCell>
+                  <TableCell align="right">{item.heart_rate}</TableCell>
+                  <TableCell align="right">{item.sys}</TableCell>
+                  <TableCell align="right">{item.dia}</TableCell>
+                  <TableCell align="right">
+                    {checkCondition(item.sys, item.dia)}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Modal>
     </div>
   );
 };
